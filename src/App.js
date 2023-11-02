6deefe73f9a7a1755c1c849f4aa88cf7c5d09c31
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-02 17:38:53 +0100

    create logo and import assets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar'
+
 function App() {
-	return <div className="text-3xl">Hello</div>
+	return (
+		<div className="">
+			<Navbar />
+		</div>
+	)
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import Logo from '../assets/logo-PW (2).png'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0e2241] text-gray-300">
+			<div>
+				<img src={Logo} alt="Logo PW" style={{ width: '60px' }} />
+			</div>
+		</div>
+	)
 }
 
 export default Navbar
