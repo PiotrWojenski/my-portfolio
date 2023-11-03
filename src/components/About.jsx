28c3fad857a35bd38d3b8266b97925c5fe895261
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-03 21:25:15 +0100

    create about component

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<div name="about" className="w-full h-screen text-gray-300 bg-gradient-to-r from-[#0e2241] to-[#1e3b69]">
+			<div className="flex flex-col justify-center items-center w-full h-full">
+				<div className="max-w-[1000px] w-full px-4 grid grid-col gap-8">
+					<div className="sm:text-right pb-8 pl-4">
+						<p className="text-4xl font-bold inline border-b-4 border-[#11AD72]">About</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default About
