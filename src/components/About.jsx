712eba8e702fa563741754dcedc000e33a1965be
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-05 22:12:19 +0100

    change styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ const About = () => {
 					</div>
 					<div></div>
 				</div>
-				<div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
-					<div className=" sm:text-right text-4xl font-bold">
+				<div className="max-w-[1000px] w-full  sm:grid grid-cols-2 gap-8 px-4">
+					<div className=" sm:text-right text-4xl font-bold ">
 						<p>Hi! I'm Piotr, nice to meet you. Please take a look around</p>
 					</div>
 					<div>
