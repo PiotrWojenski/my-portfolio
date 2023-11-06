5de6fdef3408bccc7f400350fe64e2979c24badb
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-06 15:45:58 +0100

    import skills icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import javascript from '../assets/javascript.png'
+import react from '../assets/react.png'
+import tailwind from '../assets/tailwind.png'
+import github from '../assets/github.png'
 
 const Skills = () => {
 	return <div>Skills</div>
