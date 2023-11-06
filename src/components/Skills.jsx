9c90fbdc812bea8f62ddebe23bba033ffa4b6738
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-06 16:02:56 +0100

    style skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,17 +8,17 @@ import github from '../assets/github.png'
 
 const Skills = () => {
 	return (
-		<div name="skills">
+		<div name="skills" className="bg-gradient-to-r from-[#0e2241] to-[#1e3b69] text-gray-300">
 			{/*Container*/}
-			<div>
-				<div>
-					<p>Experience</p>
-					<p> These are technologies O've worked with:</p>
+			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+				<div className="mb-4">
+					<p className="text-4xl font-bold inline border-b-4 border-[#11AD72]">Experience</p>
+					<p> These are technologies I've worked with:</p>
 				</div>
 
 				<div>
 					<div>
-						<img src={HTML} alt="html icon" />
+						<img className="w-20 mx-auto" src={HTML} alt="html icon" />
 					</div>
 				</div>
 			</div>
