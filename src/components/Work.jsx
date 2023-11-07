a65af98422064772a83446436d9d8051d3df0fc7
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-07 12:42:27 +0100

    Add work project section to Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import workProject from '../assets/workProject.png'
 
 const Work = () => {
-	return <div className="bg-gradient-to-r from-[#0e2241] to-[#1e3b69] text-gray-300 ">Work</div>
+	return (
+		<div name="work" className="w-full md:h-screen bg-gradient-to-r from-[#0e2241] to-[#1e3b69] text-gray-300 ">
+			<div className="max-w-[1000px" mx-auto p-4 flex flex-col justify-center w-full h-full>
+				<div className="pb-8">
+					<p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#11AD72]">Work</p>
+					<p className="py-6">Check out some of my recent work</p>
+				</div>
+
+				<div style={{ backgroundImage: `url(${workProject})` }} className="grid dm:grid-cols-2 md:grid-cols-3 gap-4">
+					<div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
+						{/*Hover Effects*/}
+						<div>
+							<span></span>
+							<div>
+								<a href="/">
+									<button></button>
+								</a>
+								<a href="/">
+									<button></button>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Work
