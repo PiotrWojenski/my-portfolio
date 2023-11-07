e5b3291687a9e8304efe05caaf4e817c1e5a4e07
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-07 13:05:44 +0100

    Fix styling issues in Work component and update hover effect in index.css

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import workProject from '../assets/workProject.png'
 const Work = () => {
 	return (
 		<div name="work" className="w-full md:h-screen bg-gradient-to-r from-[#0e2241] to-[#1e3b69] text-gray-300 ">
-			<div className="max-w-[1000px" mx-auto p-4 flex flex-col justify-center w-full h-full>
+			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 				<div className="pb-8">
 					<p className="text-4xl font-bold inline border-b-4 text-gray-300 border-[#11AD72]">Work</p>
 					<p className="py-6">Check out some of my recent work</p>
@@ -13,14 +13,20 @@ const Work = () => {
 				<div style={{ backgroundImage: `url(${workProject})` }} className="grid dm:grid-cols-2 md:grid-cols-3 gap-4">
 					<div className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div">
 						{/*Hover Effects*/}
-						<div className="opacity-0 group:hover-opacity-100">
-							<span></span>
-							<div>
+						<div className="opacity-0 group-hover:opacity-100">
+							<span className="text-2xl font-bold text-white tracking-wider">React JS Aplication</span>
+
+							<div className="pt-8 text-center">
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+										Demo
+									</button>
 								</a>
+
 								<a href="/">
-									<button></button>
+									<button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+										Code
+									</button>
 								</a>
 							</div>
 						</div>
