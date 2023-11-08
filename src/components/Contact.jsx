52d16fa4aeb81fdc86fee22310b354c359a2afe8
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-08 10:10:20 +0100

    Add contact form inputs and button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,18 @@ const Contact = () => {
 					<p className="text-4-xl font-bold inline border-b-4 border-[#11AD72] text-gray-300">Contact</p>
 					<p className="text-gray-300 py-4">Submit the form below or send me an emaik - pwojenskip@gmail.com</p>
 				</div>
-				<input type="text" />
+				<input type="text" placeholder="Name" name="name" className=" p-2 rounded-xl bg-[#ccd6f6]" />
+				<input className="my-4 p-2 rounded-xl bg-[#ccd6f6]" type="text" placeholder="Email" name="name" />
+				<textarea
+					className="bg-[#ccd6f6] p-2 rounded-xl"
+					placeholder="Message..."
+					name="message"
+					id=""
+					cols="30"
+					rows="10"></textarea>
+				<button className="text-white border-2 hover:bg-[#11AD72] hover:border-[#11AD72] px-8 py-3 my-8 mx-auto flex items-center rounded-xl">
+					Send
+				</button>
 			</form>
 		</div>
 	)
