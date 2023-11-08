403d0bf1a1d66832ce0bdeeff1dbca57299c5032
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-08 10:19:11 +0100

    Update contact form action URL

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
 	return (
 		<div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
-			<form action="" className="flex flex-col max-w-[600px] w-full">
+			<form
+				action="https://getform.io/f/ea70ec25-97d9-4769-9acd-c93174873440"
+				className="flex flex-col max-w-[600px] w-full">
 				<div className="pb-8">
 					<p className="text-4-xl font-bold inline border-b-4 border-[#11AD72] text-gray-300">Contact</p>
 					<p className="text-gray-300 py-4">Submit the form below or send me an emaik - pwojenskip@gmail.com</p>
