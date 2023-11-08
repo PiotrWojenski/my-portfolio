1bd99bd8cb11da001839e1cffd3e157b4f29dbc0
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-08 10:49:28 +0100

    Update Navbar links to use React Router Links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,11 +16,36 @@ const Navbar = () => {
 			{/* menu*/}
 
 			<ul className="hidden md:flex ">
-				<li className="hover:text-[#11AD72] duration-200">Home</li>
-				<li className="hover:text-[#11AD72] duration-200">About</li>
-				<li className="hover:text-[#11AD72] duration-200">Skills</li>
-				<li className="hover:text-[#11AD72] duration-200">Work</li>
-				<li className="hover:text-[#11AD72] duration-200">Contact</li>
+				<li className="hover:text-[#11AD72] duration-200">
+					{' '}
+					<Link to="home" smooth={true} duration={500}>
+						Home
+					</Link>
+				</li>
+				<li className="hover:text-[#11AD72] duration-200">
+					{' '}
+					<Link to="about" smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className="hover:text-[#11AD72] duration-200">
+					{' '}
+					<Link to="skills" smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li className="hover:text-[#11AD72] duration-200">
+					{' '}
+					<Link to="work" smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li className="hover:text-[#11AD72] duration-200">
+					{' '}
+					<Link to="contact" smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 
 			{/* Hamburger*/}
@@ -33,17 +58,33 @@ const Navbar = () => {
 				className={
 					!showMenu
 						? 'hidden'
-						: '  absolute top-0 left-0 w-full h-screen bg-transparent  flex flex-col justify-center items-center'
+						: '  absolute top-0 left-0 w-full bg-[#0a192f] h-screen flex flex-col justify-center items-center'
 				}>
 				<li className="py-5 text-5xl">
-					<Link to="home" smooth={true} duration={500}>
+					<Link onClick={handleClick} to="home" smooth={true} duration={500}>
 						Home
 					</Link>
 				</li>
-				<li className="py-5 text-5xl">About</li>
-				<li className="py-5 text-5xl">Skills</li>
-				<li className="py-5 text-5xl">Work</li>
-				<li className="py-5 text-5xl">Contact</li>
+				<li className="py-5 text-5xl">
+					<Link onClick={handleClick} to="about" smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className="py-5 text-5xl">
+					<Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+						Skills
+					</Link>
+				</li>
+				<li className="py-5 text-5xl">
+					<Link onClick={handleClick} to="work" smooth={true} duration={500}>
+						Work
+					</Link>
+				</li>
+				<li className="py-5 text-5xl">
+					<Link to="contact" smooth={true} duration={500}>
+						Contact
+					</Link>
+				</li>
 			</ul>
 
 			{/* Social icons*/}
