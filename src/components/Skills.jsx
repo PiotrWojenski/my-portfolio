de399ea887807f10e9a557c481c09b915ccfbfbc
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-10 21:03:19 +0100

    Update Skills component text

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ const Skills = () => {
 			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
 				<div className="mb-4">
 					<p className="text-4xl font-bold inline border-b-4 border-[#11AD72]">Skills</p>
-					<p className="py-5"> These are technologies I've worked with:</p>
+					<p className="py-5"> These are technologies I've worked with...</p>
 				</div>
 
 				<div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
