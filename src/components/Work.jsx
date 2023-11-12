1ee6dbb8ac63947601ede64fd4c3d5d8a7e03233
Piotr Wojenski <pwojenskip@gmail.com>
2023-11-12 22:25:14 +0100

    Update padding in Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,7 +4,7 @@ import workProject from '../assets/workProject.png'
 const Work = () => {
 	return (
 		<div name="work" className="w-full md:h-screen bg-gradient-to-r bg-[#0a192f] text-gray-300 ">
-			<div className="max-w-[1000px] mx-auto p-6 flex flex-col justify-center w-full h-full ">
+			<div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full ">
 				<div className="pb-8">
 					<p className="text-4xl font-bold inline border-b-4 mt-8 text-gray-300 border-[#11AD72]">Work</p>
 					<p className="pt-2 pb-4">Check out some of my recent work</p>
